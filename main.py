55bee8ff986a3fdbb54032bedbb3945a6c1a8a65
Sasha82Gusev <sasha82@rambler.ru>
2022-09-06 01:07:07 +0400

    second_edition

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import datetime
 import json
+import configparser
 
 
 class VK:
@@ -14,6 +15,12 @@ class VK:
     def photos(self, owner):
        url = 'https://api.vk.com/method/photos.get'
        params = {'owner_id': owner, 'album_id': 'profile', 'count': ftl, 'photo_sizes': True, 'extended': 1}
+       response = requests.get(url, params={**self.params, **params})
+       return response.json()
+
+    def info(self, user_name):
+       url = 'https://api.vk.com/method/users.get'
+       params = {'user_ids': user_name}
        response = requests.get(url, params={**self.params, **params})
        return response.json()
 
@@ -35,44 +42,50 @@ class YandexDisk:
         params = {"path": dirname}
         response = requests.put(upload_url, headers=headers, params=params)
 
+if __name__ == "__main__":
+    dict = {}
+    list1 = []
+    percent = 0
 
-dict = {}
-list1 = []
-ftl = input('Какое количество фото надо загрузить? ')
-if ftl == '':
-    ftl = 5
-owner = input('Введите user_id пользователя ВК ')
-percent = 0
-user_id = ''  # user_id владельца токена ВК
-YA_TOKEN = ""  # Токен Яндекса
-vk_access_token = ''  # Токен ВКонтакте
-ya = YandexDisk(token=YA_TOKEN)
-vk = VK(vk_access_token, user_id)
+    config = configparser.ConfigParser()  # создаём объекта парсера
+    config.read("token.ini")  # читаем конфиг
+    user_id = '17698940'  # user_id владельца токена ВК
+    YA_TOKEN = config["Tokens"]["yatoken"]  # Токен Яндекса
+    vk_access_token = config["Tokens"]["vktoken"]  # Токен ВКонтакте
+    ya = YandexDisk(token=YA_TOKEN)
+    vk = VK(vk_access_token, user_id)
+
+    ftl = input('Какое количество фото надо загрузить? ')
+    if ftl == '':
+        ftl = 5
+    owner = input('Введите user_id или user_name пользователя ВК ')
+    if str.isnumeric(owner) == False:
+        owner = str((vk.info(owner)['response'][0]['id']))
 
 
-a = vk.photos(owner)
-fotocount = a['response']['count']  # колличествоо фоток у пациента
-if fotocount > int(ftl):
-    fotocount = int(ftl)
-for photonumber in range(fotocount):
-    timestamp = a['response']['items'][photonumber]['date']  # дата загрузки в ВК в UNIX формате
-    dat = (datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d-%H-%M-%S'))
-    likes = a['response']['items'][photonumber]['likes']['count']  # колличество лайков
-    maxsize = len(a['response']['items'][photonumber]['sizes'])  # кол-во размеров фотографии
-    url = a['response']['items'][photonumber]['sizes'][maxsize - 1]['url']  # ссылка на фотографию
+    a = vk.photos(owner)
+    fotocount = a['response']['count']  # колличествоо фоток у пациента
+    if fotocount > int(ftl):
+        fotocount = int(ftl)
+    for photonumber in range(fotocount):
+        timestamp = a['response']['items'][photonumber]['date']  # дата загрузки в ВК в UNIX формате
+        dat = (datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d-%H-%M-%S'))
+        likes = a['response']['items'][photonumber]['likes']['count']  # колличество лайков
+        maxsize = len(a['response']['items'][photonumber]['sizes'])  # кол-во размеров фотографии
+        url = a['response']['items'][photonumber]['sizes'][maxsize - 1]['url']  # ссылка на фотографию
 
-    if str(likes) in dict.keys():  # формируем словарь из имен файлов и ссылки(url)
-        dict[str(likes)+'-'+dat] = url
-        list1.append({"filename": str(likes)+'-'+dat, "size": maxsize})
-    else:
-        dict[str(likes)] = url
-        list1.append({"filename": str(likes), "size": maxsize})
+        if str(likes) in dict.keys():  # формируем словарь из имен файлов и ссылки(url)
+            dict[str(likes)+'-'+dat] = url
+            list1.append({"filename": str(likes)+'-'+dat, "size": maxsize})
+        else:
+            dict[str(likes)] = url
+            list1.append({"filename": str(likes), "size": maxsize})
 
-with open('file_info.txt', 'w') as json_file:
-    json.dump(list1, json_file)
-ya.mkdir(str(owner))
-for i in dict:
-    ya.urlaa(i, dict[i])
-    percent += 1
-    print(str(int(100/fotocount*int(percent))), '%')  # Прогресс бар
-print('Загрузка завершена, загружено ',fotocount,' фото.')
+    with open('file_info.json', 'w') as json_file:
+        json.dump(list1, json_file)
+    ya.mkdir(str(owner))
+    for i in dict:
+        ya.urlaa(i, dict[i])
+        percent += 1
+        print(str(int(100/fotocount*int(percent))), '%')  # Прогресс бар
+    print('Загрузка завершена, загружено ',fotocount,' фото.')
